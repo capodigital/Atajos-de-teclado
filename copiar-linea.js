07c47f084ba29c418efa4932db0ed1eeedeb5513
capodigital <capodigitalapp@gmail.com>
2024-09-29 20:45:00 -0400

    add

--- copiar-linea.js
+++ copiar-linea.js
@@ -1,4 +1,5 @@
 // Coloca el cursor en cualquier parte de esta línea y presiona Ctrl+C
 console.log("Hola, Comunidad Capo Digital!");
+console.log("Bienvenidos");
 
 // Ahora presiona Ctrl+V en la línea siguiente para duplicarla
